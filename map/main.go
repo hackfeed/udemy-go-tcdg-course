59b17e3376ff4add98e2a25aaf2c26e1eec8da50
Sergey Kononenko <hackfeed@yandex.ru>
2020-01-30 23:09:02 +0300

    Introduction to maps in Go

---
+++ map/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+
+	modifyMap(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
+
+func modifyMap(c map[string]string) {
+	c["black"] = "#000000"
+}
