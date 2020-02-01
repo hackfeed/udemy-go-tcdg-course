3842081cca629ee030c4d7ed58a759a93deef458
Sergey Kononenko <hackfeed@yandex.ru>
2020-02-01 19:45:12 +0300

    Add noneffective curl utility written in Go

---
+++ channels/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	links := []string{
+		"https://google.com",
+		"https://facebook.com",
+		"https://stackowerflow.com",
+		"https://golang.org",
+		"https://amazon.com",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down")
+		return
+	}
+
+	fmt.Println(link, "is up!")
+}
